080d8a677f98ccc126dce86fc5a14d53ab0be518
Denis Sokolov <vzletayu@inbox.com>
2022-05-29 00:25:54 +0300

    all done

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String value;
+
+    public String getMessage() {
+        return value;
+    }
+
+    public void setMessage(String value) {
+        this.value = value;
+    }
+}
